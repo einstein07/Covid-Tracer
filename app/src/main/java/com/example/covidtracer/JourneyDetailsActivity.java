de0353d1be37d6833017957c573ffae6b9d8ae2c
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-04-27 18:58:49 +0200

    Added new layout Journey details

---
+++ app/src/main/java/com/example/covidtracer/JourneyDetailsActivity.java
@@ -0,0 +1,22 @@
+package com.example.covidtracer;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JourneyDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_journey_details);
+
+    }
+
+    public void nextEntry(View view) {
+    }
+
+    public void prevEntry(View view) {
+    }
+}
